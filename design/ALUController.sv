c4714afed55fff4e0f93234849d478d7309a5e43
Maria Clara <mclarar.almeida@gmail.com>
2025-11-21 18:34:28 -0300

    feat: add `bge` instruction

--- design/ALUController.sv
+++ design/ALUController.sv
@@ -15,6 +15,7 @@ module ALUController (
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) ||  // R\I->>> aritmetico
       ((ALUOp == 2'b10) && (Funct3 == 3'b100) && (Funct7 == 7'b0000000)) ||  // R\I-xor
       ((ALUOp == 2'b01) && (Funct3 == 3'b100)) ||  // BLT
+      ((ALUOp == 2'b01) && (Funct3 == 3'b101)) || // BGE
       ((ALUOp == 2'b10) && (Funct3 == 3'b010));    // R\I-slt
 
   assign Operation[1] = (ALUOp == 2'b00) ||  // LW\SW
@@ -23,6 +24,7 @@ module ALUController (
       ((ALUOp == 2'b10) && (Funct3 == 3'b000) && (Funct7 == 7'b0100000)) ||  // R\I-sub
       ((ALUOp == 2'b01) && (Funct3 == 3'b001)) ||  // BNE
       ((ALUOp == 2'b01) && (Funct3 == 3'b100)) ||  // BLT
+      ((ALUOp == 2'b01) && (Funct3 == 3'b101)) || // BGE
       ((ALUOp == 2'b10) && (Funct3 == 3'b010));    // R\I-slt      
 
   assign Operation[2] =  ((ALUOp==2'b10) && (Funct3==3'b101) && (Funct7==7'b0000000)) || // R\I->>
@@ -34,6 +36,7 @@ module ALUController (
 
   assign Operation[3] = ((ALUOp == 2'b01) && (Funct3 == 3'b000)) ||  // BEQ
       ((ALUOp == 2'b01) && (Funct3 == 3'b001)) || // BNE
+      ((ALUOp == 2'b01) && (Funct3 == 3'b101)) || // BGE
       ((ALUOp == 2'b10) && (Funct3 == 3'b010) && (Funct7 != 7'b0000000)) || // R\I-<
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000));  // R\I->>>
       
--- design/alu.sv
+++ design/alu.sv
@@ -37,6 +37,8 @@ module alu#(
                     ALUResult = $signed(SrcA) >>> SrcB[4:0];
             4'b1010:        // Not equal
                     ALUResult = (SrcA == SrcB) ? 0 : 1;
+            4'b1011:        // BGE
+                    ALUResult = ($signed(SrcA) >= $signed(SrcB)) ? 1 : 0;        
             default:
                     ALUResult = 0;
             endcase
